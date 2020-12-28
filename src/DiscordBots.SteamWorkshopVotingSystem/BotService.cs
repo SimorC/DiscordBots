81a40f8a295b575e708b32404fa8c5f021c50e4f
Rafael Simor <rafaelsimor@gmail.com>
2020-12-28 00:30:47 +0000

    Initial Commit

---
+++ src/DiscordBots.SteamWorkshopVotingSystem/BotService.cs
@@ -0,0 +1,68 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using DiscordBots.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBots.SteamWorkshopVotingSystem
+{
+    internal class BotService
+    {
+        private readonly ulong _channelId = GlobalVars.TestTextChannelId;
+
+        internal static Task CreateLog(LogMessage logMsg)
+        {
+            // TODO: Create proper log file
+            Console.WriteLine(logMsg);
+            return Task.CompletedTask;
+        }
+
+        internal async Task MessageReceived(SocketUserMessage message, DiscordSocketClient client)
+        {
+            int argPos = 0;
+
+            var correctChannel = message.Channel.Id == _channelId;
+            var correctPrefix = message.HasStringPrefix(GlobalVars.SteamWorkshopPrefix, ref argPos);
+            var isBot = message.Author.IsBot;
+
+            var action = (correctChannel, correctPrefix, isBot) switch
+            {
+                (true, true, _) => AddVotingEmojis(message),
+                (true, false, false) => DeleteMessage(message, client),
+                _ => Task.CompletedTask
+            };
+
+            await action;
+        }
+
+        private static async Task AddVotingEmojis(SocketUserMessage message)
+        {
+            await message.AddReactionAsync(new Emoji("🥵"));
+            await message.AddReactionAsync(new Emoji("🥶"));
+        }
+
+        private async Task DeleteMessage(SocketUserMessage message, DiscordSocketClient client)
+        {
+            var observedChannel = client.GetChannel(_channelId) as IMessageChannel;
+
+            await DeleteMessage(message, 0);
+
+            var messageAsync = await observedChannel.SendMessageAsync($"Only steam workshop links allowed, {message.Author.Username}!");
+
+            await DeleteMessage(messageAsync, 4000);
+        }
+
+        private static async Task DeleteMessage(IUserMessage message, int delay)
+        {
+            await Task.Run(() =>
+            {
+                System.Threading.Thread.Sleep(delay);
+                message.DeleteAsync();
+            });
+        }
+    }
+}
---
+++ src/DiscordBots.SteamWorkshopVotingSystem/Main.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using DiscordBots.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DiscordBots.SteamWorkshopVotingSystem
+{
+    public class Main
+    {
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+        private BotService _botService;
+
+        public Main()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService(new CommandServiceConfig { DefaultRunMode = RunMode.Async });
+            _botService = new BotService();
+
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+        }
+
+        public async Task StartBot()
+        {
+            _client.Log += ClientLog;
+
+            await RegisterCommandsAsync();
+
+            await _client.LoginAsync(TokenType.Bot, GlobalVars.VotingSystemToken);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task ClientLog(LogMessage arg)
+            => BotService.CreateLog(arg);
+
+        public async Task RegisterCommandsAsync()
+        {
+            _client.MessageReceived += MessageReceivedAsync;
+            await _commands.AddModulesAsync(Assembly.Load("DiscordBots.SteamWorkshopVotingSystem"), _services);
+        }
+
+        private async Task MessageReceivedAsync(SocketMessage arg)
+            => await _botService.MessageReceived(arg as SocketUserMessage, _client);
+    }
+}
